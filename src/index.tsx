9c4def8851630eabf684d34987ffe0c61a7b7836
Christopher Dalziel <53733670+ChristopherDalziel@users.noreply.github.com>
2024-05-24 05:26:12 +0000

    update packages, update rendering root

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./CSS/index.css";
 import App from "./Components/App";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as HTMLElement
+const root = createRoot(rootElement)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
